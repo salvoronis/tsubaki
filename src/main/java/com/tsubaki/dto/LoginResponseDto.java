301abfc13057d7fd476cc596188ccd9d57f56faf
Roman Yaremko <iaremkoro@xplanet.ru>
2023-01-08 16:58:18 +0100

    added getters and setters

--- src/main/java/com/tsubaki/dto/LoginResponseDto.java
+++ src/main/java/com/tsubaki/dto/LoginResponseDto.java
@@ -1,7 +1,12 @@
 package com.tsubaki.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class LoginResponseDto {
     private String token;
     private String type = "Bearer";
@@ -18,51 +23,4 @@ public class LoginResponseDto {
         this.roles = roles;
     }
 
-    public String getToken() {
-        return token;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
 }
--- src/main/java/com/tsubaki/dto/RegisterRequestDto.java
+++ src/main/java/com/tsubaki/dto/RegisterRequestDto.java
@@ -3,9 +3,13 @@ package com.tsubaki.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
+@Getter
+@Setter
 public class RegisterRequestDto {
 
     @NotBlank
@@ -21,28 +25,4 @@ public class RegisterRequestDto {
     @Size(min = 6, max = 40)
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
